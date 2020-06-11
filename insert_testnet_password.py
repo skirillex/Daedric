5316629c598d6d439f89826b654239e9b476d357
Kirill <kmasyche@nyit.edu>
2020-06-11 13:41:57 -0400

    Daedric docker now deploys and funds itself

--- insert_testnet_password.py
+++ insert_testnet_password.py
@@ -8,12 +8,12 @@ print(password)
 
 with open("config/yeouido/tbears_cli_config.json") as json_file:
     data = json.load(json_file)
-    print("before insert: ")
-    print(data)
+    #print("before insert: ")
+    #print(data)
     data['password'] = password
 
-    print("\n after insert:")
-    print(data)
+    #print("\n after insert:")
+    #print(data)
 
 with open("config/yeouido/tbears_cli_config.json", "w") as outfile:
     json.dump(data, outfile)
